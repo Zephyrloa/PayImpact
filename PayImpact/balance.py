ccbebdb14d6ecc25046b19df145531d50f34076a
Alis_fair <167068222+Zephyrloa@users.noreply.github.com>
2024-07-13 22:26:45 +0900

    balance.py を更新

--- PayImpact/balance.py
+++ PayImpact/balance.py
@@ -1,6 +1,6 @@
 def paypay_get_balance(session):
     if not session.access_token:
-        raise PayPayLoginError("まずはログインしてください")
+        raise PayPayLoginError("ログインしてください")
     
     response = session.session.get("https://www.paypay.ne.jp/app/v1/bff/getBalanceInfo", headers=session._headers(), proxies=session.proxy)
     data = response.json()
