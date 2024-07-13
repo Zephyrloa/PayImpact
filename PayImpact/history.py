dd549df3a64df248c65806a0b06c4f7683cbaa7c
Alis_fair <167068222+Zephyrloa@users.noreply.github.com>
2024-07-13 22:27:05 +0900

    history.py を更新

--- PayImpact/history.py
+++ PayImpact/history.py
@@ -1,6 +1,6 @@
 def paypay_get_history(session):
     if not session.access_token:
-        raise PayPayLoginError("まずはログインしてください")
+        raise PayPayLoginError("ログインしてください")
     
     response = session.session.get("https://www.paypay.ne.jp/app/v2/bff/getPay2BalanceHistory", headers=session._headers(), proxies=session.proxy)
     data = response.json()
